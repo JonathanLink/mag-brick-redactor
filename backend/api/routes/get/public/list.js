7ff033a4072a7ef3239e70c805ca4065d50a02dc
Jonathan Link <jonathan.link@swissquote.ch>
2018-02-11 04:06:34 +0000

    fix p

--- backend/api/routes/get/public/list.js
+++ backend/api/routes/get/public/list.js
@@ -2,7 +2,7 @@ const Boom = require('boom')
 const Article =  require('../../../models/article')
 
 module.exports.list = async function list(request, h) {
-
+    console.log("public list")
     try {
         let articles = await Article.find({isPosted: true}, {content: 0} ).sort({updated: -1})
         return h.response(articles)
